6e926301c8e54c7da229a44912502d04ca93e7af
pavel.uxd <44015330+paveluxd@users.noreply.github.com>
2026-05-26 23:06:17 +0300

    1

--- main.js
+++ main.js
@@ -66,6 +66,12 @@ let galleryContent = {
         style: 'width: 1000px;',
         class: 'full-width'
     },
+    'areas': {
+        src: 'areas-1280.jpg',
+        alt: 'An illustration of a combat game scene.',
+        style: 'width: 1000px;',
+        class: 'full-width'
+    },
     'scribe-cover': {
         src: 'scribe-1280x720.jpg',
         alt: 'An illustration of a combat game scene.',
