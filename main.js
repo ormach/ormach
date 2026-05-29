ec5ed4130a13f76c0c0031596918d96f575bcd9e
pavel.uxd <44015330+paveluxd@users.noreply.github.com>
2026-05-29 23:27:20 +0300

    1

--- main.js
+++ main.js
@@ -24,6 +24,111 @@ function el(id){
     return document.getElementById(id)
 }
 
+function generateNav(){
+
+    el('desktop-nav').innerHTML = `
+        <div id="nav-wrapper">
+
+            <!-- Logo -->
+            <a href="/index.html" class="logo-container">
+                <img class="logo" src="../img/orm-logo.svg" alt="Ormach logo">
+            </a>
+            
+            <!-- Buttons -->
+            <div class="link-container">
+                <a class="link-btn" href="/index.html">
+                    <img src="../img/ico/black/view.svg">
+                    Gallery
+                </a>
+                <a class="link-btn" href="../projects">
+                    <img src="../img/ico/black/repair.svg">
+                    Projects
+                </a>
+                <a class="link-btn" href="../about">
+                    <img src="../img/ico/black/unit.svg">
+                    About
+                </a>
+                
+                <!-- <a class="link-btn" target="_blank" href="https://discord.gg/AT8KdVbcVT"> 
+                    <img src="../img/ico/black/discord.svg">
+                    Discord
+                </a> -->
+
+                <a class="link-btn" target="_blank" href="https://ormach.itch.io/">
+                    <img src="../img/ico/black/shop.svg">
+                    Shop
+                </a>
+            </div>
+
+            <p id="email">pav.ormach@gmail.com</p>
+        </div>
+    ` 
+
+    el('mobile-nav').innerHTML=`
+        <button class="nav-button" onclick="toggleMenu()">
+
+            <img class="logo" src="../img/orm-logo.svg" alt="Ormach logo">
+            
+            <div class="menu-btn">
+                <img src="../img/ico/menu.svg">
+            </div>
+
+        </button>
+
+        <div class="mobile-links">
+            <a class="link-btn" href="/index.html">
+                <img src="../img/ico/black/view.svg">
+                Gallery
+            </a>
+
+            <a class="link-btn" href="../projects">
+                <img src="../img/ico/black/repair.svg">
+                Project list
+            </a>
+
+            <a class="link-btn" href="../about">
+                <img src="../img/ico/black/unit.svg">
+                About
+            </a>
+
+            <!-- <a class="link-btn" target="_blank" href="https://discord.gg/AT8KdVbcVT"> 
+                <img src="../img/ico/black/discord.svg">
+                <p style="width: 100%;">Discord server</p>
+                <img src="../img/ico/arrow.svg" style="opacity: 1;">
+            </a> -->
+
+            <a class="link-btn" target="_blank" href="https://ormach.itch.io/">
+                <img src="../img/ico/black/shop.svg">
+                    <p style="width: 100%;">Shop (itch.io)</p>
+                <img src="../img/ico/arrow.svg" style="opacity: 1;">
+            </a>
+
+            <button class="link-btn" onclick="toggleMenu()">
+                <img src="../img/ico/close.svg">
+                
+            </button>
+        </div>
+    `
+}
+
+function generateFooter(){
+    el('footer').innerHTML = `
+        <img class="logo" src="../img/orm-footer-logo.png" alt="Ormach logo">
+
+        <p>
+            If you have any questions, send me a note to pav.ormach@gmail.com<br> 
+            or find me on Discord. Lets make you game better! 
+        </p>
+
+        <div class="footerButtonContainer">
+            <a class="link-btn btn-2 ico" href="../about"><img src="../img/ico/black/discord.svg"></a>
+            <a class="link-btn btn-2" href="../about">Learn more about ORMACH -></a>
+        </div>
+
+        <div class="footerBackground"></div>
+    `
+}
+
 function generateGallery() {
     for (const [key, value] of Object.entries(galleryContent)) {
         console.log(key);        // "a", "b", "c"
